88eb91f5adeecf3c07edcfee7a789296f905a5aa
Swarnim1311 <swarnimcode13@gmail.com>
2026-07-30 08:41:45 +0530

    Time: 12 ms (43.13%) | Memory: 14.2 MB (57.39%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        freq = Counter(nums)
+        heap = []
+    
+        for num, count in freq.items():
+            heapq.heappush(heap, (count, num))
+
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        return [num for count, num in heap]
+        
